ebfed7a0a92a467e3fb7144c9812c2d8543b4314
Rohan Reddy <rohanreddy888@gmail.com>
2024-03-12 12:31:38 +0530

    OG Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,45 @@ import "./globals.css";
 
 const league = League_Spartan({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title:
     "CoinCraft - Unlock a World of Possibilities with Our Bundled Brilliance!",
   description:
     "Collect, Connect, Conquer! Elevate your experience with our curated token bundles – your key to a universe of possibilities, all in one seamless package.",
+  openGraph: {
+    title:
+      "CoinCraft - Unlock a World of Possibilities with Our Bundled Brilliance!",
+    url: "https://coincraft-interface.vercel.app",
+    description:
+      "Collect, Connect, Conquer! Elevate your experience with our curated token bundles – your key to a universe of possibilities, all in one seamless package.",
+    images: [
+      {
+        url: "https://www.quillaudits.com/og/og-home.png",
+        secureUrl: "https://www.quillaudits.com/og/og-home.png",
+        alt: "CoinCraft - Unlock a World of Possibilities with Our Bundled Brilliance!",
+        width: 1200,
+        height: 630,
+        type: "image/png",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://coincraft-interface.vercel.app",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title:
+      "CoinCraft - Unlock a World of Possibilities with Our Bundled Brilliance!",
+    description:
+      "Collect, Connect, Conquer! Elevate your experience with our curated token bundles – your key to a universe of possibilities, all in one seamless package.",
+    creator: "@QuillAudits",
+    images: ["https://www.quillaudits.com/og/og-home.png"],
+  },
+  robots: {
+    index: true,
+  },
 };
 
 export default function RootLayout({
